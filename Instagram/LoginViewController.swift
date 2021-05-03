95930081f068fa41605a5907c4f386404ab35d5e
Vidya Raghvendra Admin <VidyaRaghvendraAdmin@c02dd085ml87.lan>
2021-05-03 13:15:27 -0700

    Instagram first commit

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Vidya Raghvendra Admin on 5/3/21.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
