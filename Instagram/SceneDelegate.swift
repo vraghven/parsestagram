7f1f192d4adbaa852a6871e5bedd6fa30a2ac444
Vidya Raghvendra Admin <VidyaRaghvendraAdmin@c02dd085ml87.lan>
2021-05-17 16:52:25 -0700

    parsetagram pt 2

--- Instagram/SceneDelegate.swift
+++ Instagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,7 +18,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+        }
     }
+    
+    
+    
+    
+    
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
@@ -47,6 +60,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
+    
+    
+    
+    
+    
+    /*
+     PFUser.logOut()
+     let main = UIStoryboard(name: "Main", bundle: nil)
+     let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+     
+     let delegate = UIApplication.shared.delegate as! AppDelegate
+     
+     delegate.window.rootViewController = loginView
+     
+     
+     
+     */
 }
 
